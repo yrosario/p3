955b6e4bfda0879066bd97829dcc040016e9e3d3
Yussel Rosario <yusselrosario89@gmail.com>
2015-06-27 15:22:01 -0400

    2nd upload

--- p3.py
+++ p3.py
@@ -8,6 +8,10 @@ Created on Sun Jun 21 23:00:08 2015
 from os import listdir
 import sys
 import json
+#import pandas as pd
+import re
+import pdb
+ 
 
 def main():
     dataFolder = "data"
@@ -20,17 +24,20 @@ def main():
      #Read all JSON files.
     #Create a list of instances; note that the same instance may be listed in more than one file;
     #all instances on your list must be unique by instance ID.
-    instancesLst = getInstances(fileList)
+    instancesLst = getInstances(fileList[0:6])
     
     #Create a list of all persons; the persons are entities of type "Person"; note that the same
     #person may be listed in more than one file; all persons on your list must be unique by 
     #entity ID.
-    personsLst = getPersons(fileList)
+    #print(fileList[4:5])
+    personsLst = getPersons(fileList[4:6])
+    print(type(personsLst[:6]))
     
+    personCount = getCount(instancesLst, personsLst)
     #Count the number of persons by year. Result should be a table of N rows and M columns.
     #N is the number of persons and M is the years
     #Maybe it should return an array with?
-    personCount = getCount(instancesLst, personsLst)
+    #personCount = getCount(instancesLst, personsLst)
     
     #Normalize each column by dividing each number by the sum of all numbers in the column 
     #to calculate the frequencies of mentioning.
@@ -62,7 +69,7 @@ def main():
     #ordered by the cluster ID and then by the persons' names.
     saveToFile(clusters,clusterFile, fileType = "csv")
     
-    print(len(personsLst))
+ 
     
 
     
@@ -80,17 +87,25 @@ def getFiles(folderPath):
 #all instances on your list must be unique by instance ID.    
 def getInstances(fileList):
     listIt = []
+    instanceIds = []
     
     for file in fileList:
         try:
            path = "data/" + file
            with open(path) as infile:
                data = json.load(infile)
-           listIt.append(data["instances"])
+               content = data["instances"]
+               listIt.append(content)
            
-        except:
-            print("Unable to open file")
+        except IOError as io:
+            print("Unable open file " + io)
+            infile.close()
             sys.exit()
+        except:
+            print("Unable to open file instances")
+            infile.close()
+            sys.exit()
+            
     return listIt
     
 #Create a list of all persons; the persons are entities of type "Person"; note that the same
@@ -98,6 +113,7 @@ def getInstances(fileList):
 #entity ID.    
 def getPersons(fileList):
     listIt= []
+    personIds = []
     for file in fileList:
         try:
             path = "data/" + file
@@ -105,11 +121,14 @@ def getPersons(fileList):
                 data = json.load(infile)
             
             for key in data["entities"]:
-                if(data["entities"][key]["type"] == "Person"):
-                     listIt.append(key)
+                if data["entities"][key]["type"] == "Person" and key not in personIds:
+                     personIds.append(key)
+                     #print(personIds)
+                     listIt.append(data["entities"][key]["name"])
+                     #print(listIt)
             
         except:
-            print("Unable to open file")
+            print("Unable to open file person")
             sys.exit()
             
     return listIt
@@ -119,7 +138,53 @@ def getPersons(fileList):
 #Maybe it should return an array with?
 def getCount(lstOfInstances, lstOfPersons):
     print("Place holder")
-    return -1
+    perFreq = {}
+    #print(lstOfInstances)
+    date = ""
+    for ins in lstOfInstances:
+        if(len(ins)):
+            content = ins.pop()
+            date = content["document"]["published"][:4]
+            k = ins.pop()
+            for key in k.keys():
+               #print(key)
+               for person in lstOfPersons:
+                   #perFreq["year"] = date
+                   #perFreq["number of Persons"] = person
+                   if type(k[key]) is dict:
+                       for key2 in k[key].keys():
+                          #print(type(k[key][key2]))
+                          if type(k[key][key2]) is dict:
+                              for key3 in k[key][key2]:
+                                  #print(type(k[key][key2][key3]))
+                                  if type(k[key][key2][key3]) is str:
+                                      try:
+                                          if re.match(person, k[key][key2][key3], re.I):
+                                              perFreq["year"] = date
+                                              perFreq["number of Persons"] = 1
+                                      except:
+                                          continue
+                          elif type(k[key][key2]) is str:
+                              try:
+                                  if re.match(person, k[key][key2], re.I):
+                                      perFreq["year"] = date
+                                      perFreq["number of Persons"] = perFreq["number of Persons"] + 1
+                              except:
+                                     continue
+                   elif type(k[key]) is str and type(person) is str and len(k[key]):
+                       
+                       #pdb.set_trace()
+                       #print(k[key])
+                       try:
+                           if re.match(person, k[key], re.I):
+                               perFreq["year"] = date
+                               perFreq["number of Persons"] = 1
+                       except:
+                           continue
+                   
+                   
+    print(perFreq)     
+    return date
 
 
 #Normalize each column by dividing each number by the sum of all numbers in the column 
